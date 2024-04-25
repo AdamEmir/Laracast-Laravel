8eb8b15e41d7bab3f291f521b1995bc54a228351
Adam Emir <adamemir15@gmail.com>
2024-04-25 11:00:17 +0800

    button layout, edit, update, delete
    create button layout, edit form, update job data, delete/destroy job data.

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -1,13 +1,13 @@
 <x-layout>
     <x-slot:heading>
-        Create Job
+        Edit Job : {{$job->title}}
     </x-slot:heading>
-    <form method="POST" action="/jobs">
+    <form method="POST" action="/jobs/{{$job->id}}">
         @csrf
+        {{--server can't read PATCH method but laravel can so we declare method PATCH in the POST form--}}
+        @method('PATCH')
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
-                <h2 class="text-base font-semibold leading-7 text-gray-900">Create a New Job</h2>
-                <p class="mt-1 text-sm leading-6 text-gray-600">We just need a handful of details from you.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <div class="sm:col-span-4">
@@ -15,16 +15,17 @@
                         <div class="mt-2">
                             <div
                                 class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                {{--                                user input title--}}
+                                {{--user input title--}}
                                 <input type="text"
                                        name="title"
                                        id="title"
                                        autocomplete="title"
                                        class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 "
                                        placeholder="Shift Leader"
+                                       value="{{$job->title}}"
                                        required>
                             </div>
-                            {{--                            show error message for title--}}
+                            {{--show error message for title--}}
                             @error('title')
                             <p class="text-xs text-red-500 font-semibold mt-1">{{$message}}</p>
                             @enderror
@@ -36,15 +37,16 @@
                         <div class="mt-2">
                             <div
                                 class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                {{--                                user input salary--}}
+                                {{--user input salary--}}
                                 <input type="text"
                                        name="salary"
                                        id="salary"
                                        class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                        placeholder="RM50,000 per year"
+                                       value="{{$job->salary}}"
                                        required>
                             </div>
-                            {{--                            show error message for salary--}}
+                            {{--show error message for salary--}}
                             @error('salary')
                             <p class="text-xs text-red-500 font-semibold mt-1">{{$message}}</p>
                             @enderror
@@ -64,14 +66,25 @@
                 </div>
             </div>
         </div>
+        <div class="mt-6 flex items-center justify-between gap-x-6">
+            <div class="flex items-center">
+                <button class="text-red-500 text-sm font-bold"
+                        form="delete-form">Delete
+                </button>
+            </div>
 
-        <div class="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-            <button type="submit"
-                    class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                Save
-            </button>
+            <div class="flex items-center gap-x-6">
+                <a href="/jobs/{{$job->id}}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+                <button type="submit"
+                        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Save
+                </button>
+            </div>
         </div>
     </form>
-
+    <form method="POST" action="/jobs/{{$job->id}}" id="delete-form" class="hidden">
+        {{--server can't read DELETE method but laravel can so we declare method DELETE in the POST form--}}
+        @csrf
+        @method('DELETE')
+    </form>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ use App\Models\Job;
 */
 
 
-
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function ()  {
+//Show all job
+Route::get('/jobs', function () {
 
     $jobs = Job::with('employer')->latest()->Paginate(3);
 
@@ -29,31 +29,77 @@ Route::get('/jobs', function ()  {
     ]);
 });
 
-Route::get('/jobs/create', function ()  {
+//Show job form
+Route::get('/jobs/create', function () {
 
     return view('jobs.create');
 });
 
-Route::get('/jobs/{id}', function ($id)  {
+//Show single job
+Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
 
-Route::post('/jobs', function(){
+//Store job
+Route::post('/jobs', function () {
     //validation
     request()->validate([
         'title' => ['required', 'min:3'],
         'salary' => ['required']
     ]);
-    //insert in db
+    //store the job
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
         'employer_id' => 1,
     ]);
 
+    return redirect('/jobs');
+});
+
+//Edit job
+Route::get('/jobs/{id}/edit', function ($id) {
+
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+//Update job
+Route::patch('/jobs/{id}', function ($id) {
+    //validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+    //authorize (on hold....)
+    //search laravel route model binding
+    //update the job
+    $job = Job::findOrFail($id);
+
+    $job->title = request('title');
+    $job->salary = request('salary');
+//    $job->save();
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+    //redirect to the job page
+    return redirect('/jobs/' . $job->id);
+});
+
+//Destroy job
+Route::delete('/jobs/{id}', function ($id) {
+    //authorize (on hold)
+    //delete the job
+//    $job = Job::findOrFail($id);
+//    $job->delete();
+    Job::findOrFail($id)->delete();
+    //redirect
     return redirect('/jobs');
 });
 
