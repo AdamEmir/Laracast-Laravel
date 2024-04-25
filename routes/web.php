29abaac5ff9f1b6e369ffe8beb0b8f70bc916571
Adam Emir <adamemir15@gmail.com>
2024-04-25 15:58:44 +0800

    create JobController and organize routing
    in JobController have function index, show, create, store, edit, update and destroy. In route folder we organize the routing code.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,96 +14,17 @@ use App\Models\Job;
 |
 */
 
+Route::view('/', 'home');
 
-Route::get('/', function () {
-    return view('home');
+Route::controller(JobController::class)->group(function () {
+    Route::get('jobs', 'index');
+    Route::get('/jobs/create', 'create');
+    Route::get('/jobs/{job}', 'show');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/{job}/edit', 'edit');
+    Route::patch('/jobs/{job}', 'update');
+    Route::delete('/jobs/{job}', 'destroy');
 });
 
-//Show all job
-Route::get('/jobs', function () {
-
-    $jobs = Job::with('employer')->latest()->Paginate(3);
-
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
-
-//Show job form
-Route::get('/jobs/create', function () {
-
-    return view('jobs.create');
-});
-
-//Show single job
-Route::get('/jobs/{id}', function ($id) {
-
-    $job = Job::find($id);
-
-    return view('jobs.show', ['job' => $job]);
-});
-
-//Store job
-Route::post('/jobs', function () {
-    //validation
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-    //store the job
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1,
-    ]);
-
-    return redirect('/jobs');
-});
-
-//Edit job
-Route::get('/jobs/{id}/edit', function ($id) {
-
-    $job = Job::find($id);
-
-    return view('jobs.edit', ['job' => $job]);
-});
-
-//Update job
-Route::patch('/jobs/{id}', function ($id) {
-    //validate
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-    //authorize (on hold....)
-    //search laravel route model binding
-    //update the job
-    $job = Job::findOrFail($id);
-
-    $job->title = request('title');
-    $job->salary = request('salary');
-//    $job->save();
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]);
-    //redirect to the job page
-    return redirect('/jobs/' . $job->id);
-});
-
-//Destroy job
-Route::delete('/jobs/{id}', function ($id) {
-    //authorize (on hold)
-    //delete the job
-//    $job = Job::findOrFail($id);
-//    $job->delete();
-    Job::findOrFail($id)->delete();
-    //redirect
-    return redirect('/jobs');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::view('/contact', 'contact');
 
